679a23f4c015ac520640275ac2a8480d878a0874
DanielKorban <d.lima@acad.ifma.edu.br>
2022-04-24 00:33:08 -0300

    refatoração de js e atualização de folhas de estilo

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,11 @@ var vezJogador = vencedor = null;
 var jogadorSelecionado = document.getElementById('jogador-selecionado');
 var jogadorVencedor = document.getElementById('vencedor-selecionado');
 var quadrados = document.getElementsByClassName('quadrado');
+// simbolos da serie round six
+var triagulo = '&#9651;';
+var circulo = '&#9673;';
 
-mudarJogador('X');
+mudarJogador(triagulo);
 
 function escolherQuadrado(id){
     // ver se ja temos um ganhador
@@ -13,23 +16,24 @@ function escolherQuadrado(id){
     if(quadrado.innerHTML != '-') return;
     quadrado.innerHTML = vezJogador;
     // Estilização segundo a vez do Jogador
-    quadrado.style.color = (vezJogador == 'X')? 'green' : 'red';
+    quadrado.style.color = (vezJogador == triagulo)? 'rgb(24, 145, 88)' :  '#d40554';
     // Alternar vez dos jogadores
-    vezJogador = (vezJogador == 'X')? 'O' : 'X';
+    vezJogador = (vezJogador == triagulo)? circulo : triagulo;
     mudarJogador(vezJogador);
     checarVencedor();
 }
 
 function reiniciar(){ 
+    var corPadrao = 'rgb(241, 187, 70)';
     vencedor = null;
     jogadorVencedor.innerHTML = ''
 
     for(let i = 0; i < quadrados.length; i++){
         quadrados[i].innerHTML = '-';
-        quadrados[i].style.color = 'rgb(241, 187, 70)';
-        quadrados[i].style = 'background-color: rgb(241, 187, 70)';
+        quadrados[i].style.color = corPadrao;
+        quadrados[i].style = `background-color: ${corPadrao}`;
     }
-    mudarJogador('X');
+    mudarJogador(triagulo);
 }
 
 function mudarJogador(valor){
@@ -37,58 +41,42 @@ function mudarJogador(valor){
     jogadorSelecionado.innerHTML = vezJogador;
 }
 
+
+
 function checarVencedor(){
-    var quadrado1 = document.getElementById(1);
-    var quadrado2 = document.getElementById(2);
-    var quadrado3 = document.getElementById(3);
-    var quadrado4 = document.getElementById(4);
-    var quadrado5 = document.getElementById(5);
-    var quadrado6 = document.getElementById(6);
-    var quadrado7 = document.getElementById(7);
-    var quadrado8 = document.getElementById(8);
-    var quadrado9 = document.getElementById(9);
-    // verificar linhas
-    if(checarSequencia(quadrado1, quadrado2, quadrado3)){
-        mudarCorQuadrado(quadrado1, quadrado2, quadrado3);
-        mudarVencedor(quadrado1);
-        return true;
+    let quad = quadrados;
+    // verificar Colunas
+    let j = 0;
+    while(j < 3){
+        if(checarSequencia(quad[j], quad[j + 3], quad[j + 6])){
+            mudarCorQuadrado(quad[j], quad[j + 3], quad[j + 6]);
+            mudarVencedor(quad[j]);
+            return true;
+        }
+        j++;
     }
-    if(checarSequencia(quadrado4, quadrado5, quadrado6)){
-        mudarCorQuadrado(quadrado4, quadrado5, quadrado6);
-        mudarVencedor(quadrado4);
-        return true;
-    }
-    if(checarSequencia(quadrado7, quadrado8, quadrado9)){
-        mudarCorQuadrado(quadrado7, quadrado8, quadrado9);
-        mudarVencedor(quadrado7);
-        return;
-    }
-    // verificar colunas
-    if(checarSequencia(quadrado1, quadrado4, quadrado7)){
-        mudarCorQuadrado(quadrado1, quadrado4, quadrado7);
-        mudarVencedor(quadrado1);
-        return;
-    }
-    if(checarSequencia(quadrado2, quadrado5, quadrado8)){
-        mudarCorQuadrado(quadrado2, quadrado5, quadrado8);
-        mudarVencedor(quadrado2);
-        return;
-    }
-    if(checarSequencia(quadrado3, quadrado6, quadrado9)){
-        mudarCorQuadrado(quadrado3, quadrado6, quadrado9);
-        mudarVencedor(quadrado3);
-        return;
-    }
-    // verificar diagonais
-    if(checarSequencia(quadrado1, quadrado5, quadrado9)){
-        mudarCorQuadrado(quadrado1, quadrado5, quadrado9);
-        mudarVencedor(quadrado1);
-        return;
-    }
-    if(checarSequencia(quadrado3, quadrado5, quadrado7)){
-        mudarCorQuadrado(quadrado3, quadrado5, quadrado7);
-        mudarVencedor(quadrado3);
-        return;
+    // i+=3 seria um gap especial. i < 7(para controle do gap i para que não exceda o tamanho máximo de elementos da matriz)
+    for(let i=0; i < 7; i+=3){
+        // verificar linhas
+        if(checarSequencia(quad[i], quad[i + 1], quad[i + 2])){
+            mudarCorQuadrado(quad[i], quad[i + 1], quad[i + 2]);
+            mudarVencedor(quad[i]);
+            return true;
+        }
+        if(i < 3){// condição para evitar verificações desnecessárias.
+            // verificar diagonal principal
+            if(checarSequencia(quad[i], quad[i + 4], quad[i + 8])){
+                mudarCorQuadrado(quad[i], quad[i + 4], quad[i + 8]);
+                mudarVencedor(quad[i]);
+                return true;
+            }
+            // verificar diagonal secundaria
+            if(checarSequencia(quad[i + 2], quad[i + 4], quad[i + 6])){
+                mudarCorQuadrado(quad[i + 2], quad[i + 4], quad[i + 6]);
+                mudarVencedor(quad[i + 2]);
+                return true;
+            }
+        }
     }
 
 }
@@ -100,9 +88,8 @@ function mudarVencedor(quadrado){
 }
 
 function mudarCorQuadrado(q1, q2, q3){
-    q1.style = 'background-color: rgb(30, 172, 30)';
-    q2.style = 'background-color: rgb(30, 172, 30)';
-    q3.style = 'background-color: rgb(30, 172, 30)';
+    const bgColor = 'background-color: rgb(30, 172, 30)';
+    q2.style = q1.style = q3.style = bgColor;
 }
 
 function checarSequencia(q1, q2, q3){
